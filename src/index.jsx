ec8619e37f127183f6c6b8b073902ad19e0f64ad
Dzmitry Tsebruk <mikhama@tut.by>
2020-05-28 17:47:21 +0300

    feat: [9-97] learn about useState()

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <h1>Hello</h1>;
+const App = () => <HookSwitcher />;
+
+const HookSwitcher = () => {
+  const [color, setColor] = useState('white');
+  const [fontSize, setFontSize] = useState(14);
+
+  return (
+    <div style={{
+      fontSize,
+      padding: '10px',
+      backgroundColor: color,
+    }}
+    >
+      <p>Font Size</p>
+      <button type="button" onClick={() => setColor('gray')}>Dark</button>
+      <button type="button" onClick={() => setColor('white')}>Light</button>
+      <button type="button" onClick={() => setFontSize((s) => s + 2)}>Inc Font Size</button>
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
