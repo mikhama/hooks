03ec65bd8c3c62d47439830ace1fbf13e93a1947
Dzmitry Tsebruk <mikhama@tut.by>
2020-05-29 11:20:52 +0300

    feat: [9-101] learn how to load data using useEffect()

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React, { Component, useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState(1);
   const [visible, setVisible] = useState(true);
 
   if (visible) {
@@ -10,31 +10,44 @@ const App = () => {
       <div>
         <button type="button" onClick={() => setValue((v) => v + 1)}>+</button>
         <button type="button" onClick={() => setVisible(false)}>hide</button>
-        <Notification />
+        <PlanetInfo id={value} />
       </div>
     );
   }
   return <button type="button" onClick={() => setVisible(true)}>show</button>;
 };
 
-const HookCounter = ({ value }) => {
-  useEffect(() => {
-    console.log('mount');
-    return () => console.log('unmount');
-  }, []);
-
-  return <p>{value}</p>;
-};
-
-const Notification = () => {
-  const [message, setMessage] = useState('hello');
+const PlanetInfo = ({ id }) => {
+  const [name, setName] = useState('');
 
   useEffect(() => {
-    const timeoutId = setTimeout(() => setMessage(''), 1500);
-    return () => clearTimeout(timeoutId);
-  }, []);
+    let cancelled = false;
 
-  return <p>{message}</p>;
+    const fetchData = async () => {
+      const res = await fetch(`https://swapi.dev/api/planets/${id}`);
+      const { name: planetName } = await res.json();
+      if (!cancelled) {
+        setName(planetName);
+      }
+    };
+
+    fetchData();
+
+    // eslint-disable-next-line no-return-assign
+    return () => cancelled = true;
+  }, [id]);
+
+  return (
+    <div>
+      <p>
+        {id}
+        {' '}
+        -
+        {' '}
+        {name}
+      </p>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
